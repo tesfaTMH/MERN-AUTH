c889d691a6f6b26f78d2997bb471b531c6184737
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-07-31 17:25:36 +0300

    ready for deploying

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import path from "path";
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,6 +20,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready"));
+}
 
 app.get("/", (req, res) => res.send("Welcome to MERN Authentication"));
 
